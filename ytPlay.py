8689936ace2409c19a6e838733186cdd33f39d0b
trengere <trengere@users.noreply.github.com>
2016-06-08 21:59:41 +0200

    Create ytPlay.py

---
+++ ytPlay.py
@@ -0,0 +1,38 @@
+from browser import window
+from browser import timer
+
+def lineToList(streng):
+    viId, interval = streng.strip().split()
+    res = []
+    for v in interval.split('-'):
+        if '.' in v:
+            if v.count('.') == 1:
+                minut, sek = [int(t) for t in v.strip().split('.')]
+                res.append(sek + 60*minut)
+            else:
+                hour, minut, sek = [int(t) for t in v.strip().split('.')]
+                res.append(sek + 60*minut + 3600*hour)
+        else:
+            res.append(int(v))
+    start, end = res
+    return (viId, start, end)
+
+def unpackVids(vidStr):
+    [lineToList(v) for v in vidStr.split('\n')if v.strip()]
+
+vids = []
+
+def startVideo():
+    global vids
+    if not vids:
+        window.player.stopVideo()
+        return
+    vidId, start, end = vids.pop(0)
+    window.player.loadVideoById(vidId, start)
+    window.player.setPlaybackQuality("hd720")
+    window.player.playVideo()
+
+    timer.set_timeout(startVideo, (end-start)*1000 + 600)
+    print(window.player.getAvailableQualityLevels())
+
+timer.set_timeout(startVideo, 2000)
