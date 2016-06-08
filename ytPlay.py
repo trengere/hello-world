a6a732f3d3a89dc74aaa62165b536f96d348feca
trengere <trengere@users.noreply.github.com>
2016-06-08 22:15:12 +0200

    Update ytPlay.py
    print stuff

--- ytPlay.py
+++ ytPlay.py
@@ -24,7 +24,7 @@ vids = []
 
 def startVideo():
     global vids
-    print('inside startVideo')
+    print('inside startVideo....')
     if not vids:
         window.player.stopVideo()
         return
